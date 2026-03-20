d69c1e5d63001b4aac1d2921a6df045319e6ea99
duxa4854 <nzaicenko531@gmail.com>
2026-03-20 10:56:49 +0300

    Step 8: objects in JavaScript

--- isrpo/Lab19_LearnJs/main.js
+++ isrpo/Lab19_LearnJs/main.js
@@ -67,45 +67,44 @@ console.log(counter());
 console.log(counter());
 console.log(counter());
 
-function sumFunc(a,b){
-    return a + b;
+function sumFunc(a, b) {
+  return a + b;
 }
 
 const sumFunc2 = (a, b) => a + b;
 
-const double = x * 2;
+//const double = x * 2;
 
-const sumFunc3 = (a,b) => a+b;
+const sumFunc3 = (a, b) => a + b;
 
 const sayGreeting = () => console.log("Hello");
 
-const square = x => x * x;
-const calculate = (a,b ) => {
-    let result = a + b;
-    return result * 2;
-}
+const square = (x) => x * x;
+const calculate = (a, b) => {
+  let result = a + b;
+  return result * 2;
+};
 
-let numberArr = [1,2,3,4,5];
+let numberArr = [1, 2, 3, 4, 5];
 console.log(numberArr);
 
 console.log(numberArr[0]);
 console.log(numberArr[1]);
 
-
-let colors = ["red", "green","blue"];
+let colors = ["red", "green", "blue"];
 
 console.log("Первый элемент: ", colors[0]);
-console.log("Последний элемент: ",colors[colors.length - 1]);
+console.log("Последний элемент: ", colors[colors.length - 1]);
 
 colors[1] = "yellow";
 
 console.log("Массиы colors:", colors);
 
-let numbersArr = [1,2,3,4,5];
+let numbersArr = [1, 2, 3, 4, 5];
 console.log("Длина массива:", numbersArr.length);
 
 numbersArr.push(10);
-console.log("После рор():",numbersArr);
+console.log("После рор():", numbersArr);
 
 let student = [];
 
@@ -113,18 +112,17 @@ student.push("Alice");
 student.push("Bob");
 student.push("Charlie");
 
-
-students.pop();
+student.pop();
 
 console.log("Итоговый массив students:", student);
 
-let numbers2 = [10,20,30];
-for (let i = 0; i < numbers2.length; i++){
-    console.log(numbers2[i]);
+let numbers2 = [10, 20, 30];
+for (let i = 0; i < numbers2.length; i++) {
+  console.log(numbers2[i]);
 }
 
-for (let value of numbera2){
-    console.log(value);
+for (let value of numbers2) {
+  console.log(value);
 }
 
 let mixedArray = [1, "text", true, 3.14];
@@ -133,17 +131,88 @@ console.log(mixedArray);
 console.log(numberArr.includes(1));
 console.log(numberArr.indexOf(2));
 
+//console.log(fruits.includes("ЯБЛОКО"));
+//console.log(fruits.includes("МАНГО"));
 
-console.log(fruits.includes("ЯБЛОКО"));
-console.log(fruits.includes("МАНГО"));
-
-
-let cities = ["Москва", "Санкт-Петербург", "Казань", "Новосибирск"]
+let cities = ["Москва", "Санкт-Петербург", "Казань", "Новосибирск"];
 let cityToFind = "Казнь";
 
 let index = cities.indexOf(cityToFind);
 
-if (inndex !== -1){
-    console.log(`Город "${cityToFind}"найден под индексом ${index}`);
-    console.log(`Город"${cityToFind}" не найден`);
+if (index !== -1) {
+  console.log(`Город "${cityToFind}"найден под индексом ${index}`);
+  console.log(`Город"${cityToFind}" не найден`);
 }
+
+// практическая 1
+console.log("Книга");
+let book = {
+  title: "Война и мир",
+  author: "Лев толстой",
+  year: 1869,
+};
+console.log("Название: ", book.title);
+console.log("Автор: ", book.author);
+console.log("Год:", book.year);
+
+book.year = 2020;
+console.log("Обновляем год", book.year);
+
+// let user2 = {
+//   name1: "Ivan",
+//   age: 20,
+//   isStudent: true,
+//   sayHello: function () {
+//     console.log(`Hello, my name is "${name1}"`);
+//   },
+// };
+
+// user2.sayHello();
+// console.log(user2);
+
+// console.log(user2.name1);
+// console.log(user2.age);
+
+// console.log(user2["name"]);
+
+// практическая 2
+
+let car = {
+  brand: "Volvo ",
+  year: 2022,
+  getInfo: function () {
+    console.log(`Информация об автомобиле: ${this.brand},${this.year} года выпуска.`);
+  },
+};
+car.getInfo();
+
+// for (let key in user2) {s
+//   console.log(key + ": " + user2[key]);
+// }
+
+// практическая 3
+
+let product = {
+  name5: "Ноутбук",
+  price: 75000,
+  inStock: true,
+  category: "Электроника",
+  brand: "HP",
+};
+
+console.log("Свойства объекта product: ");
+for (let key in product) {
+  console.log(`${key}: ${product[key]}`);
+}
+
+let student3 = {
+  name: "Григорий",
+  skills: ["HTML", "CSS", "JS"],
+  address: {
+    city: "Волжский",
+    street: "Пушкина",
+  },
+};
+console.log(student3.name);
+console.log(student3.skills[0]);
+console.log(student3.address.city);
